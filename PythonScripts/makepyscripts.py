bd86c32d6459313d582d60b4b97c4f049883db89
errware <ilanbuzzetti@gmail.com>
2018-09-20 18:49:50 -0500

    removed extra line, changed terminal output

--- PythonScripts/makepyscripts.py
+++ PythonScripts/makepyscripts.py
@@ -12,12 +12,11 @@ scripts = [file for file in os.listdir(os.path.join(bat_dir,'PythonScripts')) if
 saved_dir = os.getcwd()
 os.chdir(bat_dir)
 for script in scripts:
-	print(os.path.join('PythonScripts',script))
+	print(script)
 	with open(script[0:-len(REAL_SCRIPT_EXTENSION)]+'.bat', 'w') as exeScript:
 		exeScript.write('@echo off\n')
 		# in .bat %~dp0 is the abs\directory\path\ of the script
 		# https://web.archive.org/web/20180120021145/https://www.microsoft.com/resources/documentation/windows/xp/all/proddocs/en-us/percent.mspx?mfr=true
-		exeScript.write('\n')
 		if script == me:
 			args = r'%~dp0'
 		else:
