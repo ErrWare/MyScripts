95b899e65fe4a21bdd406bb19ac9a1c65580c8d3
errware <ilanbuzzetti@gmail.com>
2018-09-21 07:36:10 -0500

    can now paste command line args (e.g. ecv hello world hi)

--- PythonScripts/ecv.py
+++ PythonScripts/ecv.py
@@ -9,15 +9,22 @@ print('Args: ' + str(len(sys.argv)))
 with open('mainmail.json','r') as mailFile:
 	mailInfo = json.load(mailFile)
 
-# TODO: change client to depend on email extension (gmail, yahoo, etc)
 smtpdomain, port = emailhelper.getSMTPDetails(mailInfo['address'])
 smtpObj = smtplib.SMTP(smtpdomain, port)
 smtpObj.ehlo()
 smtpObj.starttls()
 
 smtpObj.login(mailInfo['address'], mailInfo['password'])
-
+# determine what to paste
+if len(sys.argv) > 1:
+	text = ' '.join(sys.argv[1:])
+else
+	text = pyperclip.paste()
+print('Pasted: ' + text)
 smtpObj.sendmail(mailInfo['address'],mailInfo['address'],
-				 'Subject: copy\n'+pyperclip.paste())
+				 'Subject: copy\n'+text)
+
 smtpObj.quit()
+
+
 
